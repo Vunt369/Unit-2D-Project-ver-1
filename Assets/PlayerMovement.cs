1554b8f8a4fe1e773ffce1997a5f81dd28ab4ab9
tuan-anh-83 <117378521+tuan-anh-83@users.noreply.github.com>
2024-02-26 17:05:42 +0700

    Ground

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anmi;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
     [SerializeField] private float moveSpeed = 7f;
@@ -19,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
         anmi = GetComponent<Animator>();
     }
@@ -30,6 +34,7 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
 
+        //if (Input.GetButtonDown("Jump") && IsGrounded())
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
@@ -71,5 +76,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         anmi.SetInteger("state", (int)state);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 7f, Vector2.down, .1f, jumpableGround);
     }
 }
