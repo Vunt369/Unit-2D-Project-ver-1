73985e01c99622942add5a637a8fbd82842b5685
NhanNguyen200 <nhannq@hisoft.vn>
2024-02-28 13:24:18 +0700

    Edit map & script of enemies

--- Assets/SkeletonMovement_Nhannq.cs
+++ Assets/SkeletonMovement_Nhannq.cs
@@ -1,47 +1,41 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
 
 public class SkeletonMovement_Nhannq : MonoBehaviour
 {
-    public GameObject character;
-    public float moveSpeed = 2f;
-    public float distance = 14f;
-    public Animator animator;
-    public SpriteRenderer sprite;
-
+    public float speed = 0.5f;
+    public float distanceMoved;
     private Rigidbody2D rb;
-    private float initPosition;
-    private float endPosition;
+    public float distance;
+    private Vector3 startPosition;
+    SpriteRenderer spriteRenderer;
     void Start()
     {
+        startPosition = transform.position;
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        sprite = GetComponent<SpriteRenderer>();
-        initPosition = rb.position.x;
-        endPosition = 3f;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
-        rb.position.Set(-2.91f, -0.22f);
-        var currentPosition = rb.position.x;
-        if (currentPosition < endPosition && currentPosition > initPosition)
+        distanceMoved = Mathf.PingPong(Time.time * speed, distance);
+
+        // Di chuyển theo trục X
+        transform.position = startPosition + Vector3.right * distanceMoved;
+        flip(distanceMoved, spriteRenderer, distance);
+    }
+    void flip(float distanceMove, SpriteRenderer renderer, float distance)
+    {
+        if (distanceMove < 0.01f)
         {
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            renderer.flipX = false;
         }
-        else if (currentPosition > endPosition)
+        else if (distanceMove > distance - 0.1f)
         {
-            rb.velocity = new Vector2(moveSpeed * -1, rb.velocity.y);
-            sprite.flipX = true;
-        }
-        else if (currentPosition < initPosition)
-        {
-            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
-            sprite.flipX = true;
+            renderer.flipX = true;
         }
     }
 }
