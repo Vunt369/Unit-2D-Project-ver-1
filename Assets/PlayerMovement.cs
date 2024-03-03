576abfe71c6a7eff6b639f59e2df59d6ff682341
louisedoan <doanchaucbt@gmail.com>
2024-03-04 00:06:08 +0700

    code

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,15 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anmi;
+    private float timer = 0;
+    private bool attacking = false;
+    private float timeToAttack = 0.2f;
+    private CapsuleCollider2D capsuleCollider;
+
+    public Transform shootingPoint;
+    public GameObject bulletPrefab;
+
+    [SerializeField] private LayerMask jumpableGround;
 
     private float dirX = 0f;
-    [SerializeField] private float moveSpeed = 14f;
-    [SerializeField] private float jumpForce = 14f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 7f;
 
     private enum MovementSate { idle, running, jumping, falling }
     private MovementSate state = MovementSate.idle;
@@ -19,7 +30,9 @@ public class PlayerMovement : MonoBehaviour
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
         anmi = GetComponent<Animator>();
     }
 
@@ -30,11 +43,18 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
 
+        //if (Input.GetButtonDown("Jump") && IsGrounded())
         if (Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
 
+        /*if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+
+        }*/
+
+        ShootAnimation();
         UpdateAnimationState();
 
     }
@@ -72,4 +92,43 @@ public class PlayerMovement : MonoBehaviour
 
         anmi.SetInteger("state", (int)state);
     }
+
+    void ShootAnimation()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            anmi.SetBool("IsShoot", true);
+        }
+        else if (Input.GetButtonUp("Fire1"))
+        {
+            attacking = true;
+
+        }
+        if (attacking)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeToAttack)
+            {
+                timer = 0;
+                attacking = false;
+                anmi.SetBool("IsShoot", false);
+            }
+        }
+    }
+
+    /*private void HurtAnimation()
+    {
+        if (capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Enemies")))
+        {
+            anmi.SetBool("IsHurt", true);
+            rb.velocity = new Vector2(-27f, 5f);
+        }
+
+    }*/
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 7f, Vector2.down, .1f, jumpableGround);
+    }
 }
+
