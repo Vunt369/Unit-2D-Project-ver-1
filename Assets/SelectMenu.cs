427c2dad1140c08a406f2ae7ff024233bec971ae
NhanNguyen200 <nhannq@hisoft.vn>
2024-03-03 16:53:47 +0700

    Finish main menu & select levels

---
+++ Assets/SelectMenu.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectMenu : MonoBehaviour
+{
+    public Button[] buttons;
+    private void Awake()
+    {
+        int unlockedLevel = PlayerPrefs.GetInt("UnlockedLevel", 1);
+        Console.Write(unlockedLevel);
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            buttons[i].interactable = false;
+        }
+
+        for (int i = 0; i < unlockedLevel; i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+    public void OpenLevel(int levelId)
+    {
+        string levelName = "Level " + levelId;
+        SceneManager.LoadScene(levelName);
+    }
+}
